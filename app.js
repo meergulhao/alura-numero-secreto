e05f3898980c6e4d637312a3545d382a1b52cd89
Guilherme Mergulhão <golimpiof@gmail.com>
2024-09-30 14:06:54 -0300

    Desativar botão de chute ao acertar número

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let listaDeNumerosSorteados = [];
 let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
+document.getElementById('chutar').removeAttribute('disabled');
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -25,6 +26,7 @@ function verificarChute() {
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
+        document.getElementById('chutar').setAttribute('disabled', true);
     } else {
         if (chute > numeroSecreto) {
             exibirTextoNaTela('p', 'O número secreto é menor');
@@ -43,6 +45,7 @@ function gerarNumeroAleatorio() {
     if (quantidadeDeElementosNaLista == numeroLimite) {
         listaDeNumerosSorteados = [];
     }
+    
     if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
         return gerarNumeroAleatorio();
     } else {
@@ -63,6 +66,7 @@ function reiniciarJogo() {
     tentativas = 1;
     exibirMensagemInicial();
     document.getElementById('reiniciar').setAttribute('disabled', true)
+    document.getElementById('chutar').removeAttribute('disabled');
 }
 
 
